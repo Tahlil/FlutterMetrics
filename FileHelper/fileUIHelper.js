f0854eebcfdb68d6f986132a7d6890bdb3ca64b8
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-10-23 23:08:51 +0600

    added buttons

--- FileHelper/fileUIHelper.js
+++ FileHelper/fileUIHelper.js
@@ -1,9 +1,59 @@
-
+let currentProject, projects;
 function getFiles(elm) {
   console.log(path);
   console.log("Get all projects");
   var path = document.getElementById("myFile").files[0].path;
   ipcRenderer.send("folder-check", {multipleProject: false, path:path});
+}
+
+const getMetricButtons = function (hasOneProject) {
+  let usedMethod = hasOneProject ? "getMetricsForProject" : "getMetricsForProjects";
+  //"+usedMethod+"(" + "" + "+")"+"
+  return `<div class="row">
+    <div class="col-3 offset-1" ><button class="btn btn-info" onclick="${usedMethod}('mood')">Get MOOD metrics</button></div>
+    <div class="col-3"><button class="btn btn-primary" onclick="${usedMethod}('ck')">Get CK metrics</button></div>
+    <div class="col-3"><button class="btn btn-secondary" onclick="${usedMethod}('trd')">Get Traditional metrics</button></div>
+  </div>
+  <div class="row">
+    <div class="col-3 offset-1">
+      <p class="badge badge-info">Method Hiding Factor (MHF)</p>
+      <p class="badge badge-info">Attribute Hiding Factor (AHF)</p>
+      <p class="badge badge-info">Method Inheritance Factor (MIF)</p>
+      <p class="badge badge-info">Attribute Inheritance Factor (AIF)</p>
+      <p class="badge badge-info">Polymorphism Factor (POF)</p>
+      <p class="badge badge-info">Coupling Factor (COF)</p>
+    </div>
+    <div class="col-3">
+      <p class="badge badge-primary">Weighted Method per Class (WMC)</p>
+      <p class="badge badge-primary">Depth of Inheritance Tree (DIT)</p>
+      <p class="badge badge-primary">Number of children (NOC)</p>
+      <p class="badge badge-primary">Coupling between objects (CBO)</p>
+      <p class="badge badge-primary">Response for a Class (RFC)</p>
+      <p class="badge badge-primary">Lack of Cohesion in Methods (LCOM)</p>
+      </div>
+    <div class="col-3">
+      <p class="badge badge-secondary">McCabe Cyclomatic Complexity (CC)</p>
+      <p class="badge badge-secondary">Source Lines of Code (SLOC)</p>
+      <p class="badge badge-secondary">Comment Percentage (CP)</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-6 offset-5"><button class="btn btn-success" onclick="${usedMethod}('all')">Get ALL Metrics</button></div>
+  </div>`
+}
+
+function getMetricsForProject(metrics) {
+  console.log("metrics: " + metrics);
+  
+  console.log(currentProject);
+
+}
+
+function getMetricsForProjects(metrics) {
+  console.log("metricsssss: " + metrics);
+ 
+  console.log(projects);
+  
 }
 
 function getDartFileName(dartFiles) {
@@ -26,7 +76,8 @@ function getHTMLforOnePrj(isFlutterProject, dartFiles, projectName) {
 
 ipcRenderer.on("checked-folder", (event, res) => {
   console.log(res);
-  $("#main-section").html(getHTMLforOnePrj(res.isFlutterProject, res.dartFiles, res.projectName));
+  currentProject = {projectName: res.projectName, dartFiles: res.dartFiles}
+  $("#main-section").html(getMetricButtons(true)+  getHTMLforOnePrj(res.isFlutterProject, res.dartFiles, res.projectName));
 });
 
 const getAllProjects = function(elm){
@@ -37,11 +88,13 @@ const getAllProjects = function(elm){
 }
 
 ipcRenderer.on("checked-folders", (event, res) => {
-  let content = "";
+  let content = getMetricButtons(false);
   if(res.flutterProjects.length === 0){
     content = getHTMLforOnePrj(false, _);
   }
   else{
+    projects = {flutterProjects: [...res.flutterProjects]};
+    //console.log(projects);
     for (let index = 0; index < res.flutterProjects.length; index++) {
       const flutterProject = res.flutterProjects[index];
       content += "<span style='font-size: 2rem; font-weight: bolder;'>"+(index+1)+". </span>" + getHTMLforOnePrj(true, flutterProject.dartFiles, flutterProject.projectName);
