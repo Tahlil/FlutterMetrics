adc51e4058c16586ef1d28d01a541d03a62d29d9
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-10-23 12:37:10 +0600

    check pubspec and lib folder

--- FileHelper/fileElctronHelper.js
+++ FileHelper/fileElctronHelper.js
@@ -7,10 +7,10 @@ const walkSync = function(dir, filelist) {
     files.forEach(function(file) {
       if (fs.statSync(dir + file).isDirectory()) {
         filelist = walkSync(dir + file + '/', filelist);
-        filelist.push({name: file,isDirectory: true});  
+        filelist.push({name: file,isDirectory: true, path: dir + file});  
       }
       else {
-        filelist.push({name: file,isDirectory: false});
+        filelist.push({name: file,isDirectory: false, path: dir + file});
       }
     });
     return filelist;
@@ -20,7 +20,24 @@ const walkSync = function(dir, filelist) {
 checkFlutterPrj = function (folderPath) {
   let allFilesAndFolders = walkSync(folderPath+"/");
   console.log("All files: ");
-  console.log(allFilesAndFolders);
+  //console.log(allFilesAndFolders);
+  let hasPubspecFile=false, hasLibFolder=false, pathToPubspec, pathToLib;
+  for (let index = 0; index < allFilesAndFolders.length; index++) {
+    const file = allFilesAndFolders[index];
+    if(file.name === "lib" && file.isDirectory){
+      
+      hasLibFolder = true;
+      pathToLib = file.path;
+    }
+    else if(file.name === "pubspec.yaml" && !file.isDirectory){
+      hasPubspecFile = true;
+      pathToPubspec = file.path;
+    }
+    if(hasLibFolder && hasPubspecFile) break;
+  }
+  console.log("Pubspec: " + pathToPubspec);
+  console.log("lib: " + pathToLib) ;
+  
   return true;
 }
 
