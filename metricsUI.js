7fa1d0011046bd97275c39c159a0f42743b48bab
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-10-24 23:54:53 +0600

    table works

--- metricsUI.js
+++ metricsUI.js
@@ -1,12 +1,12 @@
 function displayMetrics(tabledata){
   var table = new Tabulator("#res-table", {
-    height:"311px",
+    //height:"311px",
     layout:"fitColumns",
     data:tabledata, //load data into table
     columns:[
         {title:"Project Name", field:"projectName", sorter:"string", width:200},
-        {title:"Predicted Gender", field:"sloc", sorter:"string"},
-        {title:"Actual Gender", field:"cp", sorter:"string"},
+        {title:"Source Lines of Code (SLOC)", field:"sloc", sorter:"string"},
+        {title:"Comment Percentage (CP)", field:"cp", sorter:"string"},
         // {title:"Success", field:"hasPredicted", formatter:"tickCross", align:"center", width:100}
     ],
 });
@@ -14,6 +14,7 @@ function displayMetrics(tabledata){
 
 ipcRenderer.on('project-metrics', (event, res) => {
   console.log(res);
+  displayMetrics(res);
   // currentProject = {projectName: res.projectName, dartFiles: res.dartFiles}
   // $("#main-section").html(getMetricButtons(true)+  getHTMLforOnePrj(res.isFlutterProject, res.dartFiles, res.projectName));
 });
